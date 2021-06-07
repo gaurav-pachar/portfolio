28a8b2d56854ce13527da134eb2caf5846267a59
Gaurav Pachar <gaurav.pachar@gmail.com>
2021-06-08 01:59:46 +0530

    Updates

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import navBaritemsChngReducer from './Redux/btnChngSlice';
-
-export default configureStore({
-    reducer: {
-        navBaritemsStateChng: navBaritemsChngReducer,
-    },
-})
